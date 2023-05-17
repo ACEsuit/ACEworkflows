4fa3423ab22dfe48d8c5ee763aa6c6aa8819ecf8
Christoph Ortner <christohortner@gmail.com>
2023-05-17 09:12:10 -0700

    fix paper figures

--- priors/plot_priors.jl
+++ priors/plot_priors.jl
@@ -2,7 +2,12 @@
 include("utils.jl")
 using ACE1pack, Plots, LaTeXStrings, ACE1
 using ACE1.Transforms: transform
+# Plots.pythonplot()
+# Plots.plotly()
 Plots.pyplot()
+default(fontfamily = "serif")
+# Plots.rc("font",family="serif")
+# Plots.gr()
 
 ##
 
@@ -19,25 +24,25 @@ rcut = 5.0
 _trans = ACE1.Transforms.agnesi_transform(r0, 6, 6)
 trans_a = r -> 1 - _trans(r)
 
-rr = rdf(dataset, rcut; ndat=100_000)
-# rr = reweight_rr(rr)
+rr = rdf(dataset, rcut; ndat=10_000)
 minr = minimum(rr)
 ra = 0.0; rb = rcut 
 xa = -0.03; xb = 1.0 
 
 xx = trans_a.(rr)
-# xx = reweight_rr(xx; f = 0.15)
 x0 = trans_a(r0)
 xmin, xmax = extrema(xx)
 
 
 
-histogram(rr, nbins=60, label = "", 
+histogram(rr, nbins=60, label = L"g(r)", legend = :topleft, 
           ytick = [], xtick = [], yflip=true, 
-          xlims = [0, rcut], lw=0, yscale = :log10, 
-          axis = false, )
+          xlims = [0, rcut], lw=0, legend_foreground_color = :white, 
+          axis = false, c = 5, )
 plt1 = vline!([r0,], lw=2, c=2,  label = "")
 plt1 = vline!([minr, 3], lw=1, c=2,  label = "")
+
+
 
 rp = range(0.000001, rcut, length=200)
 
@@ -55,17 +60,19 @@ end
 
 
 plot!(plt2, rp, trans_a.(rp), xlim = [ra, rb], ylim = [xa, xb], lw=3, 
-         label = "", ylabel = L"y(r)",  xlabel = L"r [\AA]", 
+         ylabel = L"y~~[{\rm nondim.}]",  xlabel = L"r~[\AA]", 
          xticks = ([0, 1, minr, r0, 3, 4, 5], 
                    ["0", "1", L"r_{\min}", L"r_0", "3", "4", "5"]), 
+         yticks = [], 
+         label = "", # legend = :topleft, 
          grid = false, c = 1
          )
 
 
-plt3 = histogram(xx, nbins=100, label = "", 
+plt3 = histogram(xx, nbins=100, label = L"g(y)", legend = :bottomleft, 
             orientation=:horizontal, ylim = [xa, xb], 
-            lw=0, c = 1, ytick = [],  xtick = [], 
-            xscale = :log10, xlim = [0.001, 40_000,],
+            lw=0, c = 3, ytick = [],  xtick = [], 
+            xlim = [0.001, 300,], legend_foreground_color = :white, 
             grid=false, axis=false, )
 
 hline!(trans_a.(xticks), lw=1, c=2, label = "")
@@ -86,7 +93,9 @@ savefig(@__DIR__() * "/transform.pdf")
 
 f(r) = min((r0/r)^(10) - 2*(r0/r)^5, 1_000_000)
 
-plt1 = plot(rp, f.(rp), xlabel = L"r", yticks = [], xlim = [ra, rb], ylim = [-1.5, 2], lw=2, label = L"V(r)")
+plt1 = plot(rp, f.(rp), xlabel = L"r~[\AA]", yticks = [], xlim = [ra, rb],
+                   ylim = [-1.5, 2], lw=2, label = L"V(r)", 
+                   legend_foreground_color = :white, grid = false)
 
 
 rp_ = range(0.01, rb, length = 300) 
@@ -94,16 +103,16 @@ xp_ = trans_a.(rp_)
 
 xp = trans_a.(rp)
 plt2 = plot(xp, f.(rp), xlim = [xa, xb], ylim = [-1.5, 2], 
-                  xlabel = L"y", yticks = [], 
-                  label = L"V(y)", lw = 2)
+                  xlabel = L"y~~[{\rm nondim.}]", yticks = [], xticks = [], 
+                  label = L"V(y)", lw = 2, )
 
 e_guess(r) = min((r0/r)^(3), 1_000) + (r0/r)
 plot!(plt2, xp_, f.(rp_) ./ e_guess.(rp_), 
-            label = L"V(y) / e(y)", lw = 2)
+            label = L"V(y) / f_{\mathrm{env}}(y)", lw = 2)
 
 e_ideal(r) = min((r0/r)^(10), 1_000_000) + (r0/r)
-plot!(plt2, xp_, f.(rp_) ./ e_ideal.(rp_), 
-            label = L"V(y) / e_{\mathrm{ideal}}(y)", lw=2)
+plot!(plt2, xp_, f.(rp_) ./ e_ideal.(rp_), legend_foreground_color = :white, 
+            label = L"V(y) / f_{\mathrm{env}}^{\mathrm{ideal}}(y)", lw=2)
 
 
 plot(plt1, plt2, layout = grid(1, 2, widths = [0.5, 0.5]), 
--- priors/utils.jl
+++ priors/utils.jl
@@ -2,28 +2,33 @@
 using JuLIP
 using LinearAlgebra: norm
 
-function rdf(at::Atoms, rcut::Real)
+function _rdf(at::Atoms, rcut::Real)
    nlist = neighbourlist(at, rcut)
    return [ norm(ijr[3]) for ijr in pairs(nlist) ]
 end
 
 function rdf(dataset::AbstractVector{<: Atoms}, rcut::Real; 
-             ndat = 10_000)
-   rr = rdf(dataset[1], rcut)
+             r2scale = true, ndat = 10_000)
+   rr = _rdf(dataset[1], rcut)
    for at in dataset[2:end]
-      rr = vcat(rr, rdf(at, rcut))
+      rr = vcat(rr, _rdf(at, rcut))
    end
+   
+   if r2scale 
+      rr = reweight_rr(rr)
+   end
+
    sort!(rr)
    step = length(rr) ÷ ndat
    return rr[1:step:end]
 end
 
-function reweight_rr(rr::AbstractVector{<: Real}; f = 0.25)
-   rmin = minimum(rr) 
-   rmax = maximum(rr)
+
+function reweight_rr(rr::AbstractVector{<: Real})
+   rmin, rmax = extrema(rr)
    rrnew = eltype(rr)[]
    for r in rr
-      p_accept = 1 - (1 - f) * (r - rmin) / (rmax - rmin)
+      p_accept = (rmin / r)^2
       if rand() < p_accept
          push!(rrnew, r)
       end
