ae2937d7d89b7d6f897ae97a6a2d6b3106bd1250
Chuck Witt <witt.chuck@gmail.com>
2023-08-20 17:51:16 +0100

    more with 5.

--- CodeSnippets/listing-5.jl
+++ CodeSnippets/listing-5.jl
@@ -1,5 +1,5 @@
 using ACEpotentials
 model = acemodel(elements = [:Ti, :Al],
-                 order = 4
-                 totaldegree = [25, 23, 20, 10],
-                 wL = 2.0)
+                 order = 4,
+                 wL = 2.0,
+                 totaldegree = [25, 23, 20, 10])
