3e34bfc7b073c7cbd3dfc7671f13c83e0f956137
Chuck Witt <witt.chuck@gmail.com>
2023-08-20 17:48:47 +0100

    update 5.

--- CodeSnippets/listing-5.jl
+++ CodeSnippets/listing-5.jl
@@ -1,10 +1,5 @@
-using ACE1pack
-
-@info "BEGIN LISTING 4"
-
-model = acemodel(
-            elements = [:Ti, :Al],
-            rcut = 5.5,
-            order = 4,
-            totaldegree = [25, 23, 20, 10],
-            wL = 2.0)
+using ACEpotentials
+model = acemodel(elements = [:Ti, :Al],
+                 order = 4
+                 totaldegree = [25, 23, 20, 10],
+                 wL = 2.0)
