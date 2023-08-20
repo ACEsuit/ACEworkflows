7380aebbdf8292da80ee41652a42b4efbd3793cf
Chuck Witt <witt.chuck@gmail.com>
2023-08-20 17:35:55 +0100

    update listing 3.

--- CodeSnippets/listing-3.jl
+++ CodeSnippets/listing-3.jl
@@ -1,17 +1,7 @@
-using ACE1pack
-model = acemodel(; elements = [:Ti, :Al],
-            order = 3,
-#            totaldegree = 15,
-            totaldegree = 5,
-            rcut = 5.5,
-            Eref = [:Ti => -1586.0195, :Al => -105.5954]) # 1-body parameters
+include("listing-2.jl")
 
-@info "BEGIN LISTING 2"
-
-#model = ... # cf. Listing 1
+#model = ... # cf. Listing 2
 P = smoothness_prior(model)
-pathtodata = joinpath(ACE1pack.artifact("TiAl_tutorial"), "TiAl_tutorial.xyz")
-data = read_extxyz(pathtodata)
-acefit!(model, data; solver = ACEfit.RRQR(rtol = 1e-4, P = P))
-# TODO
-#export2lammps("TiAl.yace", model)
+data, _, _ = ACEpotentials.example_dataset("TiAl_tutorial")
+acefit!(model, data; prior = P, solver = ACEfit.BLR())
+export2lammps("TiAl.yace", model)
