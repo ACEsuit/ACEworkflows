ad585ee409aef8e4fe51ccd217baa3e6d4557f80
Chuck Witt <witt.chuck@gmail.com>
2023-08-20 17:03:52 +0100

    finish updating listing-1.

--- CodeSnippets/listing-1.jl
+++ CodeSnippets/listing-1.jl
@@ -1,12 +1,10 @@
-@info "BEGIN EXAMPLE 1"
+@info "BEGIN LISTING 1"
 
-using ACE1pack
-pathtodata = joinpath(ACE1pack.artifact("TiAl_tutorial"), "TiAl_tutorial.xyz")
-data = read_extxyz(pathtodata)
-#model = acemodel(elements = [:Ti, :Al], order = 3, totaldegree = 15,
-model = acemodel(elements = [:Ti, :Al], order = 3, totaldegree = 5,
+using ACEpotentials
+data, _, _ = ACEpotentials.example_dataset("TiAl_tutorial")
+model = acemodel(elements = [:Ti, :Al],
+                 order = 3,
+                 totaldegree = 10,
                  Eref = [:Ti => -1586.0195, :Al => -105.5954])
 acefit!(model, data)
-# TODO
-# export2lammps("TiAl.yace", model)
-#ACE1pack.ExportMulti.export_ACE("potential.yace", model.potential, export_pairpot_as_table=true)
+export2lammps("TiAl_tutorial.yace", model)
