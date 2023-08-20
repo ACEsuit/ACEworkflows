e8b3b01aebdb30aced3170338967d4e181b78739
Chuck Witt <witt.chuck@gmail.com>
2023-08-20 17:25:42 +0100

    update listing 1.

--- CodeSnippets/listing-1.jl
+++ CodeSnippets/listing-1.jl
@@ -4,7 +4,7 @@ using ACEpotentials
 data, _, _ = ACEpotentials.example_dataset("TiAl_tutorial")
 model = acemodel(elements = [:Ti, :Al],
                  order = 3,
-                 totaldegree = 10,
+                 totaldegree = 12,
                  Eref = [:Ti => -1586.0195, :Al => -105.5954])
 acefit!(model, data)
 export2lammps("TiAl_tutorial.yace", model)
