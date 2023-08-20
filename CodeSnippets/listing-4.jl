5a98fd7162b5223300a93cab019646e49f6b6c13
Chuck Witt <witt.chuck@gmail.com>
2023-08-20 17:44:13 +0100

    update listing 4.

--- CodeSnippets/listing-4.jl
+++ CodeSnippets/listing-4.jl
@@ -1,15 +1,12 @@
-using ACE1pack
-
-@info "BEGIN LISTING 3"
-
+using ACEpotentials
 elements = [:Ti, :Al]
+totaldegree = 12
 r0 = (rnn(:Ti) + rnn(:Al)) / 2
-rin = 0.5 * r0
 rcut = 2 * r0
-trans = AgnesiTransform(; r0=r0, rin=rin, p = 2)
-fenv = ACE1.PolyEnvelope(1, r0, rcut)
-radbasis = transformed_jacobi_env(12, trans, fenv, rcut)
+trans = AgnesiTransform(; r0=r0, p = 2)
+fenv = PolyEnvelope(1, r0, rcut)
+radbasis = transformed_jacobi_env(totaldegree, trans, fenv, rcut)
 model = acemodel(elements = elements,
                  order = 3,
-                 totaldegree = 15,
+                 totaldegree = totaldegree,
                  radbasis = radbasis)
